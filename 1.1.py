4f931aa0b0ed6d81fbd3e84532ae9aca335a878d
kingflyfly <542001608@qq.com>
2019-07-10 09:18:35 +0800

    init

--- 1.1.py
+++
@@ -1,7 +0,0 @@
-while True:
-    try:
-        print(int(input("Please enter a number: ")))
-        break
-    except ValueError:
- #       print(111)
-        pass
--- 第17章/tengxun.py
+++
@@ -1,8 +0,0 @@
-import requests
-url = "https://graph.qq.com/user/get_user_info"
-r = requests.get(url)
-print(r.status_code)
-t = r.json()
-print(t)
-for key,value in t.items():
-       print(key,':',value)
