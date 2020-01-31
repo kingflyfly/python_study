8427eb106594e91c7c197bd6851862d9a93e2c1b
root <542001608@qq.com>
2020-01-31 11:00:32 +0800

    --

--- return语句.py
+++
@@ -1,9 +0,0 @@
-#filename is return
-def maxd(x,y):
-    if x > y:
-        return x
-    elif x == y:
-        return x
-    else :
-        return y
-print(maxd(3,4))
--- using_list.py
+++
@@ -1,6 +0,0 @@
-#filename is using_list.py
-#shoping_list
-shoplist = ["apple","mango","xihongshi"]
-print(shoplist,len(shoplist))
-shoplist1 = ("apple","mango","xihongshi")
-print(shoplist1[:])
