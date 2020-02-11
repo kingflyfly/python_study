20d6e7ef72adc0612ad4e5b869e65cb978a3c6d7
root <542001608@qq.com>
2020-02-11 14:50:22 +0800

    --

--- qq.py
+++ qq.py
@@ -1,5 +1,12 @@
-phone_number = "135 819 5813"
-print(phone_number.replace(phone_number[3:],"*" * 7))
-print("beijing\nshanghai\nnanjing")
-city = input("write down the name of city:")
-url = "http://apistore.baidu.com/microservice/weather?citypinyin={}".format(city)
+"""参数
+
+"""
+s = (1, 3, 4)
+
+
+def test(*a):
+    print(a)
+    b, c, d = a
+    return b
+
+print(test(*s))
