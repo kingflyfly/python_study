cf795a1f9352d8f5a72023eda321c72353f2405a
root <542001608@qq.com>
2020-02-17 16:00:02 +0800

    --

--- 第15章-并发编程/first_thread.py
+++ 第15章-并发编程/first_thread.py
@@ -1,16 +1,16 @@
 import threading
 
+
 def action(max):
-    for i in range(max):
-        print(threading.current_thread().getName()+" "+str(i))
+    for j in range(max):
+        print(threading.current_thread().getName() + " " + str(j))
+
 
 for i in range(100):
     print(threading.current_thread().getName() + "----" + str(i))
     if i == 20:
-        t1 = threading.Thread(target=action,args=(100,))
+        t1 = threading.Thread(target=action, args=(100,))
         t1.start()
-        t2 = threading.Thread(target=action,args=(100,))
+        t2 = threading.Thread(target=action, args=(100,))
         t2.start()
 print('主线程执行完成!')
-
-
--- 第15章-并发编程/join_thread.py
+++ 第15章-并发编程/join_thread.py
@@ -1,12 +1,15 @@
 import threading
-def action(max):
-    for i in range(max):
-        print(threading.current_thread().name+" "+str(i))
 
-threading.Thread(target=action,args=(100,),name='新线程').start()
+
+def action(max1):
+    for j in range(max1):
+        print(threading.current_thread().name + " " + str(j))
+
+
+threading.Thread(target=action, args=(100,), name='新线程').start()
 for i in range(100):
     if i == 20:
-        jt = threading.Thread(target=action,args=(100,),name="被join的线程")
+        jt = threading.Thread(target=action, args=(100,), name="被join的线程")
         jt.start()
         jt.join(timeout=None)
-    print(threading.current_thread().name+" "+str(i))
+    print(threading.current_thread().name + " " + str(i))
