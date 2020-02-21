89be166c9b8145cee5351ed38a8963c45d42d4fc
root <542001608@qq.com>
2020-02-21 18:20:19 +0800

    --

---
+++ 第15章-并发编程/继承.py
@@ -0,0 +1,18 @@
+class Person(object):
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+    def test(self):
+        print(self.a)
+
+class YanZhe(Person):
+    def __init__(self):
+        #使用未绑定方法调用父类的构造方法
+        # Person.__init__(self)
+        #通过super()函数调用父类的构造方法
+        super().__init__()
+        self.b = 3
+    def test(self):
+        print(self.b)
+t = YanZhe()
+t.test()
--- 第17章/2.py
+++ 第17章/2.py
@@ -1,6 +1,5 @@
 import requests
 url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 r = requests.get(url)
-#print("status code:",r.status_code)
 t = r.json()
 print(t)
