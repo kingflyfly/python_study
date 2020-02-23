ae842f586ff8a03d084645fb39777e32d04f1185
root <542001608@qq.com>
2020-02-23 14:44:02 +0800

    --

--- 第15章-并发编程/add_done_callback.py
+++ 第15章-并发编程/add_done_callback.py
@@ -1,6 +1,5 @@
 from concurrent.futures import ThreadPoolExecutor
 import threading
-import time
 
 
 def action(max):
@@ -10,11 +9,16 @@ def action(max):
         my_sum += i
     return my_sum
 
+
 with ThreadPoolExecutor(max_workers=2) as pool:
     future1 = pool.submit(action, 50)
     future2 = pool.submit(action, 100)
+
+
     def get_result(future):
         print(future.result())
+
+
     future1.add_done_callback(get_result)
     future2.add_done_callback(get_result)
     print('-------------------------------')
