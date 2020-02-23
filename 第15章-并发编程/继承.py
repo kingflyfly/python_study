0daef31cf437d8b30f64f681aa4ba2c1be92e2e5
root <542001608@qq.com>
2020-02-23 14:41:29 +0800

    --

--- 第15章-并发编程/继承.py
+++ 第15章-并发编程/继承.py
@@ -10,7 +10,7 @@ class YanZhe(Person):
         #使用未绑定方法调用父类的构造方法
         # Person.__init__(self)
         #通过super()函数调用父类的构造方法
-        super().__init__()
+        super(YanZhe,self).__init__()
         self.b = 3
     def test(self):
         print(self.b)
