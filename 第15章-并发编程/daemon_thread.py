23de4eca853ea7eb609485ae4e29e191626d8a59
root <542001608@qq.com>
2020-02-25 23:03:23 +0800

    --

--- 第15章-并发编程/daemon_thread.py
+++ 第15章-并发编程/daemon_thread.py
@@ -4,8 +4,8 @@ def action(max):
     for i in range(max):
         print(threading.current_thread().name+" "+str(i))
 t = threading.Thread(target=action,args=(100,),name='后台线程')
-# t.daemon = True
-t.daemon = False
+t.daemon = True
 t.start()
+
 for i in range(10):
     print(threading.current_thread().name+" "+str(i))
